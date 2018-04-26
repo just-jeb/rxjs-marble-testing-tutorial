c9a92f38bd0f1b80b8f726238eb73e165c37896c
Evgeny Barabanov <Evgeny Barabanov@philips.com>
2018-04-26 11:32:10 +0300

    Setup jest and minimal test for permissions Remove karma and jasmine

--- src/app/permissions.service.spec.ts
+++ src/app/permissions.service.spec.ts
@@ -1,15 +1,31 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
-import { PermissionsService } from './permissions.service';
+import {PermissionsService} from './permissions.service';
+jest.mock('./users.service');
+import {UsersService} from './users.service';
 
 describe('PermissionsService', () => {
+  let permissions;
+  let usersMock;
+  let httpMock;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PermissionsService]
+      imports: [HttpClientTestingModule],
+      providers: [PermissionsService, UsersService]
     });
   });
 
-  it('should be created', inject([PermissionsService], (service: PermissionsService) => {
-    expect(service).toBeTruthy();
-  }));
+  beforeEach(
+    inject([PermissionsService, UsersService, HttpTestingController], (_permissions, _usersMock, _httpMock) => {
+      permissions = _permissions;
+      usersMock = _usersMock;
+      httpMock = _httpMock;
+    }));
+
+  it('should be created', () => {
+    expect(permissions).toBeTruthy();
+    expect(usersMock).toBeTruthy();
+    expect(httpMock).toBeTruthy();
+  });
 });
